a4ec2eae4819a1cfda75662beb5e76d35e5e8464
JangTaeGyu <ttggbbgg2@gmail.com>
2024-08-11 15:51:47 +0900

    entity 변환

--- src/main/java/com/example/study/MigrationRunner.java
+++ src/main/java/com/example/study/MigrationRunner.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Component
@@ -13,10 +15,29 @@ import java.util.List;
 public class MigrationRunner implements CommandLineRunner {
     private final CsvLoader csvLoader;
 
+    private LocalDateTime stringToLocalDateTime(String date) {
+        if (date == null || date.isBlank()) {
+            return null;
+        }
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return LocalDateTime.parse(date, formatter);
+    }
+
     @Override
     public void run(String... args) throws Exception {
-        List<DummyUser> users = csvLoader.load("data.csv", record -> {
+        List<DummyUser> users = csvLoader.load("data.csv", record -> new DummyUser(
+                Long.parseLong(record.get(0)),
+                record.get(1),
+                record.get(2),
+                record.get(3),
+                record.get(4),
+                record.get(5),
+                stringToLocalDateTime(record.get(6))
+        ));
 
+        users.forEach(dummyUser -> {
+            System.out.println(dummyUser.getName());
         });
     }
 }
