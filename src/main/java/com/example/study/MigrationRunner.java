ada2bfaca588fb5f16bb4095da7869ba4ba8f532
JangTaeGyu <ttggbbgg2@gmail.com>
2024-08-12 21:56:10 +0900

    single thread with transactional 5만개 데이터 테스트 추가

--- src/main/java/com/example/study/MigrationRunner.java
+++ src/main/java/com/example/study/MigrationRunner.java
@@ -14,8 +14,8 @@ public class MigrationRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        singleThreadedSaver.save("fake_data_50k.csv");
-        // singleThreadedSaver.saveTransaction("fake_data_50k.csv");
+        // singleThreadedSaver.save("fake_data_50k.csv");
+        singleThreadedSaver.saveTransaction("fake_data_50k.csv");
         // multiThreadedSaver.save("fake_data_50k.csv", 1000);
     }
 }
